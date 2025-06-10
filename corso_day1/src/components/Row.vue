ddb37e40ce4be0baf40648a5c7ea6997b4747477
Gerardo Doro <gerrydoro97@gmail.com>
2025-06-10 16:48:20 +0200

    Aggiunto corso day 1

---
+++ corso_day1/src/components/Row.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref, reactive } from 'vue'
+const props = defineProps({
+    item:{}
+});
+
+const emit = defineEmits(['remove', 'complete'])
+
+function remove() {
+  emit('remove', props.item.idx)
+}
+
+function complete() {
+  emit('complete', props.item.idx)
+}
+
+</script>
+
+<template>
+    <tr :class="{'td-odd':true}">
+        <td>
+            {{item.idx}}
+        </td> 
+        <td>
+            {{item.title}}
+        </td> 
+        <td>
+            {{item.done}}
+        </td> 
+        <td>
+            {{item.timestamp}}
+        </td> 
+        <td>
+            {{item.user}}
+        </td>
+        <td>
+            {{item.user}}
+        </td> 
+        <td>
+            <button @click="emit('remove', item.idx)">❌</button>
+            <button v-if="!item.done" @click="emit('complete', item.idx)">✅ Completa</button>
+        </td>
+    </tr>
+</template>
+
+<style scoped>
+    .td-odd {
+        filter: blur(0px);
+    }
+    td {
+        border: 1px dashed gray;
+    }
+</style>
