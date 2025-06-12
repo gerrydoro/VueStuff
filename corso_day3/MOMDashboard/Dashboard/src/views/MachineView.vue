b6cb5b9b5ed9501b8d28a757d23b1ce897021ff8
Gerardo Doro <gerrydoro97@gmail.com>
2025-06-12 09:36:15 +0200

    Fixato mom f

---
+++ corso_day3/MOMDashboard/Dashboard/src/views/MachineView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Auxilary from '@/components/dashboard/Auxilary.vue'
+import Template from '@/components/dashboard/Template.vue'
+import Content from '@/components/dashboard/Content.vue'
+import GetWorkCenterStatus from 'momframeworkcorso3'
+</script>
+
+<template>
+  <Template>
+    <template #header> cece è bello </template>
+    <template #auxiliary>
+      <Auxilary></Auxilary>
+    </template>
+    <template #content>
+      <Content></Content>
+    </template>
+  </Template>
+</template>
